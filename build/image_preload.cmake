adafe4b7b0724be723335193775a14190cf987c2
momoisgoodforhealth <shoban.shakya@gmail.com>
2025-04-06 14:25:09 -0400

    first commit

---
+++ build/image_preload.cmake
@@ -0,0 +1,8 @@
+# Generated file that can be used to preload variant images
+set(BOARD "nrf5340dk_nrf5340_cpuapp" CACHE INTERNAL "NCS child image controlled")
+set(BOARD_ROOT "c:/ncs/v2.6.1/zephyr/samples/subsys/display/lvgl;C:/ncs/v2.6.1/nrf;C:/ncs/v2.6.1/zephyr" CACHE INTERNAL "NCS child image controlled")
+set(CACHED_CONF_FILE "C:/ncs/v2.6.1/zephyr/samples/subsys/display/lvgl/prj.conf" CACHE INTERNAL "NCS child image controlled")
+set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
+set(DTC_OVERLAY_FILE "C:/ncs/v2.6.1/zephyr/samples/subsys/display/lvgl/nrf5340dk_nrf5340_cpuapp.overlay" CACHE INTERNAL "NCS child image controlled")
+set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
+set(WEST_PYTHON "C:/ncs/toolchains/cf2149caf2/opt/bin/python.exe" CACHE INTERNAL "NCS child image controlled")
